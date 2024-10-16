4c021ccf6694f04c330986cb3ed772bedcbab063
stacey <104475305+Stubbs5647@users.noreply.github.com>
2024-10-15 20:48:39 -0400

    Update descriptiveQueries.sql
    Added remaining 3 queries and fixed syntax errors

--- P3/descriptiveQueries.sql
+++ P3/descriptiveQueries.sql
@@ -16,13 +16,13 @@ GROUP BY seller_username;
 -- Select users names with their emails
 SELECT fullname, email 
 FROM (
-    User INNER JOIN User_email ON User.username = User_email.username
+    [User] INNER JOIN User_email ON User.username = User_email.username
 );
 
 -- Select users along with their ratings
 SELECT fullname, rating
 FROM (
-    User INNER JOIN User_Rates ON User.username = rating.ratee
+    [User] INNER JOIN User_Rates ON [User].username = User_Rates.ratee
 );
 
 -- 2 subqueries
@@ -31,7 +31,7 @@ SELECT pm.method_name
 FROM PaymentMethod pm
 WHERE pm.payment_id IN (
     SELECT p.payment_id
-    FROM User_Payment p
+    FROM User_PaymentMethods p
     JOIN [User] u ON p.username = u.username
     WHERE u.isbuyer = 1
 );
@@ -47,3 +47,26 @@ SELECT fullname, AVG(rating) AS average_rating
 FROM (
     USER INNER JOIN User_Rates ON User.username = rating.ratee
 ) GROUP BY User.fullname;
+
+-- Aggregate: Select the the sum of items each seller is selling
+SELECT seller_username, MIN(price)
+FROM ListedItem
+GROUP BY seller_username
+
+-- Join: Select listedItems along with their condition
+SELECT item_id, [name], condition_name
+FROM (
+    ListedItem INNER JOIN Condition ON ListedItem.condition_id = Condition.condition_id
+);
+
+-- Subqueries: Select from the seller which have 
+SELECT u.username
+FROM [User] u
+WHERE u.isseller = 1 AND u.username IN (
+    SELECT l.seller_username
+    FROM (
+        ListedItem l INNER JOIN Condition c ON l.condition_id = c.condition_id
+    )
+    WHERE condition_name = 'New'
+);
+
