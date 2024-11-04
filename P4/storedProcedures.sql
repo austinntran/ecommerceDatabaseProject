32f3055e98d69a122c8c3e5ba5a9be7cfde4cf12
jpark0504 <justinpark04@gmail.com>
2024-11-04 12:55:58 -0500

    added procedure outlines

---
+++ P4/storedProcedures.sql
@@ -0,0 +1,8 @@
+-- 1. Insert user into database
+-- important to reduce need for multiple insert lines 
+
+-- 2. Update listed item with buyer and purchase date
+-- important to ensure item is not bought twice
+
+-- 3. Remove item from wishlist/shopping cart when bought
+-- important to ensure item can no longer be bought
