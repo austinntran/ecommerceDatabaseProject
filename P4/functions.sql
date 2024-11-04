f9294eebc36c33f7acf9e4e14420de81e4667809
stacey <104475305+Stubbs5647@users.noreply.github.com>
2024-11-04 13:46:07 -0500

    Update functions.sql
    Functions

--- P4/functions.sql
+++ P4/functions.sql
@@ -1,6 +1,34 @@
--- 1. Selects all listed items available (not already bought) in a certain category
+GO
+USE PROJECT
 
--- 2. Select all items listed by a user (identified by their email)
+-- 1. Selects all listed items available (not already bought) in a certain category
+CREATE FUNCTION GetAvailableItems
+(
+    @category varchar(50)
+)
+RETURNS TABLE
+AS 
+RETURN (
+    SELECT LC.item_id
+    FROM ListedItem_Category LC
+    JOIN Category C ON LC.category_id = C.category_id
+    WHERE @category = C.category_name
+);
+
+-- 2. Select all items listed by a username
+CREATE FUNCTION GetUserItems 
+(
+    @username varchar(100)
+) 
+RETURNS TABLE
+AS 
+RETURN (
+    SELECT *
+    FROM ListedItem LI
+    WHERE @username = LI.seller_username 
+);
+
+SELECT * FROM GetUserItems('amandabrown77');
 
 -- 3. Select all items listed after a certain date cost at most a specified price
 CREATE FUNCTION GetLatestCheapItems
