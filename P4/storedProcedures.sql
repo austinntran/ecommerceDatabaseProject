acfd974e43f24a090037ba3b6fe4b66579a52e41
jpark0504 <justinpark04@gmail.com>
2024-11-04 13:04:07 -0500

    created first procedure

--- P4/storedProcedures.sql
+++ P4/storedProcedures.sql
@@ -1,5 +1,28 @@
 -- 1. Insert user into database
 -- important to reduce need for multiple insert lines 
+CREATE PROCEDURE AddCustomer
+    @username VARCHAR(50),
+    @passhash VARCHAR(100),
+    @fullname VARCHAR(30),
+    @isbuyer BIT,
+    @isseller BIT
+AS
+BEGIN
+    IF (LEN(@username) < 5) 
+    BEGIN
+        RAISERROR('Username must be over 5 characters', 16, 1)
+        RETURN;
+    END;
+
+    IF (EXISTS (SELECT username FROM customer WHERE username=@username))
+    BEGIN
+        RAISERROR('Username already exists', 16, 1)
+        RETURN;
+    END;
+
+    INSERT INTO Customer (username, passhash, fullname, isbuyer, isseller)
+    VALUES (@username, @passhash, @fullname, @isbuyer, @isseller);
+END;
 
 -- 2. Update listed item with buyer and purchase date
 -- important to ensure item is not bought twice
